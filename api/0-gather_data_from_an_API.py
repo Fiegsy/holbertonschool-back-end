6342f9961cbae62a0c2ac12d8f3736b632b6a8e8
Fiegsy <yassine.daoui031@gmail.com>
2024-04-16 03:02:49 -0700

    Fixed missing "print" for employee's name, number of completed tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Script that retrieves information on a given employee's TODO list
-progress"""
+"""
+Script to display the progress of a given employee's TODO list.
+"""
 
 import json
 import requests
 import sys
 
-def get_employee_todo_progress(emp_id):
+def fetch_employee_todo_progress(employee_id):
     
     task_titles = []
-    number_of_done_tasks = 0
-    total_number_of_tasks = 0
+    num_completed_tasks = 0
+    total_tasks = 0
 
     
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{emp_id}")
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     user_data = user_response.json()
     employee_name = user_data.get('name')
 
@@ -23,21 +24,21 @@ def get_employee_todo_progress(emp_id):
 
     
     for task in todos:
-        if task.get('userId') == emp_id:
-            total_number_of_tasks += 1
+        if task.get('userId') == employee_id:
+            total_tasks += 1
             if task.get('completed'):
-                number_of_done_tasks += 1
+                num_completed_tasks += 1
                 task_titles.append(task['title'])
 
     
-    print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
+    print(f"Employee {employee_name} has completed {num_completed_tasks}/{total_tasks} tasks:")
     for title in task_titles:
         print(f"\t{title}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python employee_todo.py <employee_id>")
+        print("Usage: python employee_todo_progress.py <employee_id>")
         sys.exit(1)
-    
+
     employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    fetch_employee_todo_progress(employee_id)
